b0305cabd9ff1ef93c8e3c9a5284c1d86221e925
Cat125 <106539381+cat-125@users.noreply.github.com>
2024-03-10 19:17:46 +0300

    update GUI

--- src/main.js
+++ src/main.js
@@ -30,6 +30,15 @@ let cps = 0;
 let fps = 60;
 let fpsc = 0;
 
+// Configuration
+let config = {
+	circles: true,
+	cpsText: true,
+	infoText: true
+};
+
+// Config GUI
+
 const win = new cheatgui.Window({
 	title: "Configuration",
 	x: 10,
@@ -43,11 +52,15 @@ win.append(new cheatgui.Button('Save configuration', () => {
 	localStorage.setItem('acpst-cfg', JSON.stringify(win.getConfig()));
 }));
 
-// ...
+win.append((new cheatgui.Switch('Circles', true)).bind(config, 'circles'));
+win.append((new cheatgui.Switch('Large CPS text', true)).bind(config, 'cpsText'));
+win.append((new cheatgui.Switch('Show info', true)).bind(config, 'infoText'));
 
 if (localStorage.getItem('acpst-cfg')) {
 	win.loadConfig(JSON.parse(localStorage.getItem('acpst-cfg')));
 }
+
+// End of config GUI
 
 function getTime() {
 	return performance.now();
@@ -57,7 +70,7 @@ document.addEventListener('pointerdown', e => {
 	if (e.touches) e = e.touches[0];
 	[prevX, prevY] = [mouseX, mouseY];
 	[mouseX, mouseY] = [e.clientX, e.clientY];
-	circles.push({ x: mouseX, y: mouseY, life: 100 });
+	if (config.circles) circles.push({ x: mouseX, y: mouseY, life: 100 });
 	isMouseDown = true;
 	clicks++;
 	const t = getTime();
@@ -88,32 +101,38 @@ function tick() {
 	ctx.globalAlpha = 1;
 	cps = (1000 / (delaySum / clicks) || rcps).toFixed(2);
 
-	ctx.font = '25px Arial';
-	ctx.fillStyle = '#fff';
-	const m = ctx.measureText(cps + " CPS");
-	ctx.fillText(cps + ' CPS', width / 2 - m.width / 2, height / 2 + m.hangingBaseline / 2);
-	
-	ctx.font = '15px Arial';
-	const lines = [
-		'FPS: ' + fps,
-		'CPS: ' + cps,
-		'Real CPS: ' + rcps,
-		'Average Delay: ' + (Math.round(delaySum / clicks) || '0') + 'ms'
-	];
-	const h = Math.round(ctx.measureText('A').hangingBaseline);
-	for (let i = 0; i < lines.length; i++) {
-		ctx.fillText(lines[i], 10, 10 + ((h + 5) * (i + 1)));
+	if (config.cpsText) {
+		ctx.font = '25px Arial';
+		ctx.fillStyle = '#fff';
+		const m = ctx.measureText(cps + " CPS");
+		ctx.fillText(cps + ' CPS', width / 2 - m.width / 2, height / 2 + m.hangingBaseline / 2);
 	}
 
-	ctx.strokeStyle = `hsl(${frame % 360}deg 50% 50%)`;
-	for (let i = 0; i < circles.length; i++) {
-		const circle = circles[i];
-		ctx.globalAlpha = circle.life / 100;
-		ctx.beginPath();
-		ctx.arc(circle.x, circle.y, Math.round(100 - circle.life), 0, Math.PI * 2, false);
-		ctx.stroke();
-		circles[i].life -= 2.3;
-		if (circles[i].life <= 0) circles.splice(i, 1);
+	if (config.infoText) {
+		ctx.font = '15px Arial';
+		const lines = [
+			'FPS: ' + fps,
+			'CPS: ' + cps,
+			'Real CPS: ' + rcps,
+			'Average Delay: ' + (Math.round(delaySum / clicks) || '0') + 'ms'
+		];
+		const h = Math.round(ctx.measureText('A').hangingBaseline);
+		for (let i = 0; i < lines.length; i++) {
+			ctx.fillText(lines[i], 10, 10 + ((h + 5) * (i + 1)));
+		}
+	}
+
+	if (config.circles) {
+		ctx.strokeStyle = `hsl(${frame % 360}deg 50% 50%)`;
+		for (let i = 0; i < circles.length; i++) {
+			const circle = circles[i];
+			ctx.globalAlpha = circle.life / 100;
+			ctx.beginPath();
+			ctx.arc(circle.x, circle.y, Math.round(100 - circle.life), 0, Math.PI * 2, false);
+			ctx.stroke();
+			circles[i].life -= 2.3;
+			if (circles[i].life <= 0) circles.splice(i, 1);
+		}
 	}
 
 	fpsc++;
